24357c27ef9efb98053f320ee2f7f408593a67dc
francesco madonna <fra.mad.93@hotmail.it>
2024-04-30 16:44:50 +0200

    ciclo while

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ const spesa = [
     'pane', 'acqua', 'pasta', 'farina', 
     'prosciutto', 'formaggio', 'fanta', 'sugo', 
   ];
+
+
+  let i = 0;
+
+while (i < spesa.length){
+  prodotti = spesa[i];
+  document.getElementById('list').innerHTML +=`<li> ${prodotti} </li>`;
+  console.log(spesa[i]);
+  i++;
+}
